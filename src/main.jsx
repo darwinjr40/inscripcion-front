57383edcdb35260a552d20d621d6bb630c20c6a1
Javi <javier193061@gmail.com>
2023-02-24 23:29:35 -0400

    tailwind installed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Inscripcion } from "./pages/Inscripcion";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Inscripcion />
+  </React.StrictMode>
+);
---
+++ src/pages/Inscripcion.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Inscripcion = () => {
+  return (
+    <div className="border text-2xl text-red-500">
+      Hola papu página de inscripción
+    </div>
+  );
+};
